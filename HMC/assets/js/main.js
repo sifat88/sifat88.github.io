2a9e604733e8d8faceb077d8c24940c6393be369
Rakibul Islam Sifat <rakibulislam8sifat@gmail.com>
2020-09-19 18:48:06 +0600

    hms2

---
+++ HMC/assets/js/main.js
@@ -0,0 +1,96 @@
+// Isotope Customization js
+
+var iso = new Isotope( '.gbox', {
+    itemSelector: '.gbox-item',
+    stagger: 30,
+    getSortData: {
+      name: '.name',
+      category: '[data-category]'
+    },
+    masonry: {
+      columnWidth: '.gbox-sizer'
+    }
+  });
+  
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function( itemElem ) {
+      var number = itemElem.querySelector('.number').textContent;
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function( itemElem ) {
+      var name = itemElem.querySelector('.name').textContent;
+      return name.match( /ium$/ );
+    }
+  };
+  
+  // bind filter button click
+  var filtersElem = document.querySelector('.filter-button-group');
+  filtersElem.addEventListener( 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    var filterValue = event.target.getAttribute('data-filter');
+    // use matching filter function
+    filterValue = filterFns[ filterValue ] || filterValue;
+    iso.arrange({ filter: filterValue });
+  });
+  
+  // change is-checked class on buttons
+  var buttonGroups = document.querySelectorAll('.bttn-grp');
+  for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup( buttonGroup );
+  }
+  
+  function radioButtonGroup( buttonGroup ) {
+    buttonGroup.addEventListener( 'click', function( event ) {
+      // only work with buttons
+      if ( !matchesSelector( event.target, 'button' ) ) {
+        return;
+      }
+      buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+      event.target.classList.add('is-checked');
+    });
+  }
+//   * Lightbox ============= //
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.gbox-single-item')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = document.querySelectorAll('lb').src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
+
+// Counter up
+$('.counter').counterUp({
+  delay: 10,
+  time: 4000,
+});
+// Owl carousel
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:false,
+    nav:true
+});
