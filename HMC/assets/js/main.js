36d224c999a0bf43594a7c5a60bccd768efc8916
Rakibul Islam Sifat <rakibulislam8sifat@gmail.com>
2020-09-21 15:50:05 +0600

    pf

--- HMC/assets/js/main.js
+++ HMC/assets/js/main.js
@@ -57,27 +57,27 @@ var iso = new Isotope( '.gbox', {
     });
   }
 //   * Lightbox ============= //
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+// const lightbox = document.createElement('div')
+// lightbox.id = 'lightbox'
+// document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('.gbox-single-item')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = document.querySelectorAll('lb').src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-  })
-})
+// const images = document.querySelectorAll('.gbox-single-item')
+// images.forEach(image => {
+//   image.addEventListener('click', e => {
+//     lightbox.classList.add('active')
+//     const img = document.createElement('img')
+//     img.src = document.querySelectorAll('lb').src
+//     while (lightbox.firstChild) {
+//       lightbox.removeChild(lightbox.firstChild)
+//     }
+//     lightbox.appendChild(img)
+//   })
+// })
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+// lightbox.addEventListener('click', e => {
+//   if (e.target !== e.currentTarget) return
+//   lightbox.classList.remove('active')
+// })
 
 // Counter up
 $('.counter').counterUp({
