1792298c97e87a94e7c8cb45e653e336f39481d4
Rakibul Islam Sifat <rakibulislam8sifat@gmail.com>
2020-10-05 15:19:22 +0600

    Zugan4All

---
+++ Zugang4all/assets/js/main.js
@@ -0,0 +1,16 @@
+
+// Get Element
+const toggleButton1 = document.getElementById("tgbox1");
+const toggleButton2 = document.getElementById("tgbox2");
+const toggleButton3 = document.getElementById("tgbox3");
+const toggleButton4 = document.getElementById("tgbox4");
+const toggleButton5 = document.getElementById("tgbox5");
+const toggleButton6 = document.getElementById("tgbox6");
+
+// Add Function
+toggleButton1.addEventListener("change" , () => toggleButton1.classList.toggle("bgColor"));
+toggleButton2.addEventListener("change" , () => toggleButton2.classList.toggle("bgColor"));
+toggleButton3.addEventListener("change" , () => toggleButton3.classList.toggle("bgColor"));
+toggleButton4.addEventListener("change" , () => toggleButton4.classList.toggle("bgColor"));
+toggleButton5.addEventListener("change" , () => toggleButton5.classList.toggle("bgColor"));
+toggleButton6.addEventListener("change" , () => toggleButton6.classList.toggle("bgColor"));
