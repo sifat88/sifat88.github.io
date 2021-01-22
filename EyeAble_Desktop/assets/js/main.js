8fe683690c824150861308087a812d15144c120c
Rakibul Islam Sifat <rakibulislam8sifat@gmail.com>
2021-01-22 19:16:17 +0600

    EA:TEST2

--- EyeAble_Desktop/assets/js/main.js
+++ EyeAble_Desktop/assets/js/main.js
@@ -5,6 +5,22 @@ eyeAbleLaunchbtn.addEventListener("click" , ()=> {
     eyeAbleMain.classList.toggle("eyeAbleON");
 })
 
+const launchButton = document.querySelector("img.eyeAble_launchlogo");
+const launchButtonOFF = document.querySelector("img.eyeAble_launchoff");
+const turboButton = document.querySelector("button.eyeAble_turbo-button");
+
+launchButtonOFF.addEventListener('click', ()=>{
+    launchButton.parentNode.classList.add('eyeAble_launchnuttonclose')
+})
+
+launchButton.addEventListener('click', ()=>{
+    launchButton.parentNode.classList.remove('eyeAble_launchnuttonclose')
+})
+
+turboButton.addEventListener('click', ()=>{
+    turboButton.classList.toggle('eyeAble_turboactive')
+})
+
 // Single feature toggle  ========================================//
 const singleUI = document.querySelectorAll("div.eyeAble_single-feature");
 const cancelUI = document.querySelectorAll("div.eyeAble_text-wrapper > img.eyeAble_img");
@@ -29,16 +45,34 @@ const Blaufilterblk = document.getElementById("eyeAbleBlaufilterBlock");
 const Blaufilter = document.getElementById("eyeAbleBlaufilterBlock");
 const Blaufilterbg = document.getElementById("eyeAblefilteBG");
 
+
+
+Blaufilterblk.addEventListener("click", (e)=> {
+    Blaufilterblk.classList.add("eyeAbleFilterActive");
+})
+Blaufilterblk.addEventListener("mouseover", (e)=> {
+    setTimeout( ()=> {
+        Blaufilterblk.classList.remove("eyeAbleFilterActive");
+    }, 3000)
+    
+})
+
 Blaufilter.addEventListener("input", (e)=> {
     Blaufilterbg.style.width = e.target.value + '%';
+    e.target.parentNode.childNodes[5].innerHTML = "Stärke: " + e.target.value + "%";
 })
 
 const Farbschwachefilterblk = document.getElementById("eyeAbleFarbschwachefilterBlock");
 const Farbschwachefilter = document.getElementById("eyeAbleFarbschwachefilter");
 const FarbschwachefilterBG = document.getElementById("eyeAbleFarbschwachefilterBG");
 
+Farbschwachefilterblk.addEventListener("click", (e)=> {
+    Farbschwachefilterblk.classList.add("eyeAbleFilterActive");
+})
+
 Farbschwachefilter.addEventListener("input", (e)=> {
     FarbschwachefilterBG.style.width = e.target.value + '%';
+    e.target.parentNode.childNodes[5].innerHTML = "Stärke: " + e.target.value + "%";
 })
 
 // ScreenReader toggle ====================//
@@ -80,7 +114,6 @@ const colorWeeknessSliderBG = document.querySelectorAll("div.eyeAble_colorranger
 colorWeeknessSlider.forEach(singleRanger => {
     singleRanger.addEventListener("input" , (e)=> {
         e.target.nextElementSibling.style.width = e.target.value + "%";
-        console.log(e.target.parentNode.childNodes);
         e.target.parentNode.childNodes[5].innerHTML = "Stärke: " + e.target.value + "%";
     })
 })
@@ -93,6 +126,8 @@ const placeAutoButton = document.getElementById("placeAutomotionbtn");
 const timeAutoBox = document.getElementById("timeAutomotion");
 const placeAutoBox = document.getElementById("placeAutomotion");
 const automotionText = document.getElementById("automotiontext");
+
+const resetAutomotion = document.getElementById("automotionReset");
 
 timeAutoButton.addEventListener("click" , ()=> {
     if( placeAutoBox.classList.contains('eyeAble_automotion-active')){
@@ -107,4 +142,18 @@ placeAutoButton.addEventListener("click" , ()=> {
     }
     automotionBox.classList.add("eyeAble_automotion-active");
     placeAutoBox.classList.add("eyeAble_automotion-active");
+})
+resetAutomotion.addEventListener("click" , ()=> {
+    automotionBox.classList.remove("eyeAble_automotion-active");
+    timeAutoBox.classList.remove("eyeAble_automotion-active");
+    placeAutoBox.classList.remove("eyeAble_automotion-active");
+})
+
+// Adjustment slider ====================//
+const adjustmentSlider = document.getElementById("adjustSlider");
+const adjustmentSliderBG = document.querySelector('div.eyeAble_adjustmentrangerbg');
+
+
+adjustmentSlider.addEventListener("input", (e)=> {
+    adjustmentSliderBG.style.width = e.target.value + '%';
 })
